320a1d0dc6471a76d30ce642d11d22774e255402
chughraghav1 <chughraghav@seasiainfotech.com>
2023-09-02 14:11:54 +0530

    rsyncing

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/frontend_projects/landing
 npm install
 npm run build
 
-rsync /home/ubuntu/frontend_projects/landing/dist/  /var/www/html
+rsync -a /home/ubuntu/frontend_projects/landing/dist/  /var/www/html

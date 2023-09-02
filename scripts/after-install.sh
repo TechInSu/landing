55163238f09c3847442c091b1c571e34140ce2dd
chughraghav1 <chughraghav@seasiainfotech.com>
2023-09-02 15:39:38 +0530

    script changes

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+cd /home/ubuntu/frontend_projects/landing
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 18.17.1
 nvm use 18.17.1
 
-cd /home/ubuntu/frontend_projects/landing
+npm install -g yarn
 
-npm install
-npm run build
+yarn
+yarn build
 
 rsync -a /home/ubuntu/frontend_projects/landing/dist/  /var/www/html
